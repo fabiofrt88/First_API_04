e5bea2d6ff2ae22409969a046ed9de0a023cc3fb
Fabio Frattarelli <fabiofrt@outlook.it>
2023-02-10 09:04:52 +0100

    commit

--- src/main/java/com/example/First_API_04/HeaderController.java
+++ src/main/java/com/example/First_API_04/HeaderController.java
@@ -3,12 +3,14 @@ package com.example.First_API_04;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/headers")
 public class HeaderController {
 
-    @GetMapping("/headers")
+    @GetMapping("/header")
     public String getHostParam(@RequestHeader (name = "host")String host){
         return host;
     }
